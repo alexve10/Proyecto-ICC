e705014729b0831bb1763a274e67ae3d2aa544c6
alexve10 <40767818+alexve10@users.noreply.github.com>
2018-07-08 22:06:41 -0500

    Add files via upload

---
+++ 4ta Parte.py
@@ -0,0 +1,45 @@
+if dia==1:
+    print("Debe realizar la siguiente rutina: ")
+    a=random.sample(torso,2)
+    b=random.sample(cardio,2)
+    for item in a:
+        print(item)
+    for cos in b:
+        print(cos)
+    print("Repetir 2 vez")
+if dia==2:
+    print("Debe realizar la siguiente rutina")
+    a=random.sample(torso,2)
+    b=random.sample(cardio,2)
+    for item in a:
+        print(item)
+    for cos in b:
+        print(cos)
+    print("Repetir 1 vez")
+if dia==3:
+    print("Debe realizar la siguiente rutina")
+    a=random.sample(torso,2)
+    b=random.sample(cardio,2)
+    for item in a:
+        print(item)
+    for cos in b:
+        print(cos)
+    print("Repetir 1 vez")
+if dia==4:
+    print("Debe realizar la siguiente rutina")
+    a=random.sample(torso,2)
+    b=random.sample(cardio,2)
+    for item in a:
+        print(item)
+    for cos in b:
+        print(cos)
+    print("Repetir 1 vez")
+if dia==5:
+    print("Debe realizar la siguiente rutina")
+    a=random.sample(torso,2)
+    b=random.sample(cardio,2)
+    for item in a:
+        print(item)
+    for cos in b:
+        print(cos)
+    print("Repetir 1 vez")
